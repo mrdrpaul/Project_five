10edefd1d077a8a947aa95042d2fd50254d3bbbe
Paul Cruz <paul.cruz10.mil@army.mil>
2025-04-28 13:58:49 -0500

    initial project creation with backend and frontend created

---
+++ ProjectFive/src/main/java/com/bridge/example/projectfive/ProjectFiveApplication.java
@@ -0,0 +1,13 @@
+package com.bridge.example.projectfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectFiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectFiveApplication.class, args);
+    }
+
+}
---
+++ ProjectFive/src/test/java/com/bridge/example/projectfive/ProjectFiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridge.example.projectfive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
