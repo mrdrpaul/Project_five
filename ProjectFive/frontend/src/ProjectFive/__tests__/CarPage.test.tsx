4149733f355357e9072b1d25256fd054bab6a166
Greg Gomez <greg.gomez.mil@a@usmc.mil>
2025-04-30 16:24:43 -0500

    more spelling errors fixed

--- ProjectFive/frontend/src/ProjectFive/__tests__/CarPage.test.tsx
+++ ProjectFive/frontend/src/ProjectFive/__tests__/CarPage.test.tsx
@@ -8,7 +8,7 @@ describe('Car Page', () => {
 
     it('should display a fetched car', async () => {
         const expectedCar: Car[] = [
-            {id: 1, make: "Ford", model: "Mustang", year: 2017, price: 2043.33, isUsed: true},
+            {id: 1, make: "Ford", model: "Mustang", year: 2017, price: 2043.33, used: true},
         ]
 
         const mockFetchCar = vi.spyOn(CarService, "fetchCars")
